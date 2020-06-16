49b8f0cc4f9df57fd9630d4728bb53ab81ad7634
guedanie <danguerrero93@gmail.com>
2020-06-16 12:50:10 -0400

    added name page

--- server.py
+++ server.py
@@ -9,7 +9,11 @@ def index():
 
 @app.route("/roll-dice")
 def roll_dice_1():
-    n_1 = random.randint(1, 7)
-    n_2 = random.randint(1, 7)
-    n_3 = random.randint(1, 7)
-    return f"our random numbers are {str(n_1)}, {str(n_2)} and {str(n_3)}"
+    n_1 = random.randint(1, 6)
+    n_2 = random.randint(1, 6)
+    n_3 = random.randint(1, 6)
+    return f"Our random numbers are {str(n_1)}, {str(n_2)} and {str(n_3)}"
+
+@app.route("/hello/<name>")
+def sayhi(name):
+    return f"Hello, {name.title()}"
