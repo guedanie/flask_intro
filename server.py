0b548534949534358e9a101f0ce59662c9c9f5cf
guedanie <danguerrero93@gmail.com>
2020-06-16 15:35:16 -0400

    added HTML structures

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
 from flask import Flask
 import random
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World"
+    return render_template("index.html")
 
-@app.route("/roll-dice")
-def roll_dice_1():
-    n_1 = random.randint(1, 6)
+@app.route("/roll-dice/<int:n>")
+def roll_dice_1(n):
+    n_1 = sum([random.randint(1, 2) for i in range(n)])
     n_2 = random.randint(1, 6)
     n_3 = random.randint(1, 6)
-    return f"Our random numbers are {str(n_1)}, {str(n_2)} and {str(n_3)}"
+    n_4 = random.randint(1, 6)
+    return render_template("roll-dice.html", n_1 = n_1, n_2=n_2, n_3 = n_3, n_4=n_4)
 
 @app.route("/hello/<name>")
 def sayhi(name):
